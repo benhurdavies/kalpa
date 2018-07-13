630b21970e78d5be0906f454d84df06d146fecd1
Benhur Davies <benhurdavies@gmail.com>
2018-07-13 14:40:33 +0530

    clean up

--- spec/feature/assignments.spec.js
+++ spec/feature/assignments.spec.js
@@ -2,12 +2,11 @@ import RDParser from '../../lib/parser/RDParser';
 import CompilationContext from '../../lib/CompilationContext';
 import RuntimeContext from '../../lib/RuntimeContext';
 
-const program = `numeric a; a=5+6*9; printLine "benhur age is "+a;`;
+const program = `numeric a; a=20+7; printLine "benhur age is "+a;`;
 
 const parser = new RDParser(program);
 const compileContext = new CompilationContext();
 const runtimeContext = new RuntimeContext();
-debugger;
 const statements = parser.parse(compileContext);
 
 statements.forEach(statement => {
