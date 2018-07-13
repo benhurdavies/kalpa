202411e5f65beee802587e71431ee8ba9e097f4e
Benhur Davies <benhurdavies@gmail.com>
2018-07-13 16:30:25 +0530

    bool fix

--- spec/feature/assignments.spec.js
+++ spec/feature/assignments.spec.js
@@ -2,7 +2,7 @@ import RDParser from '../../lib/parser/RDParser';
 import CompilationContext from '../../lib/CompilationContext';
 import RuntimeContext from '../../lib/RuntimeContext';
 
-const program = `numeric a; a=20+7; printLine "benhur age is "+a;`;
+const program = `bool a; a=true; printLine a;`;
 
 const parser = new RDParser(program);
 const compileContext = new CompilationContext();
